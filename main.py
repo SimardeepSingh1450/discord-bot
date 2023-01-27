2fb1c51420e11c8aec9742b0e7f4e2a20bdfc427
simardeepsm <smudhar_be21@thapar.edu>
2023-01-27 15:05:36 +0530

    added profanity detect and remove

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import os
 import discord
+from better_profanity import profanity
+from dotenv import load_dotenv
+#load_dotenv from dotenv
+load_dotenv()
+
+#profanity curse words from file
+profanity.load_censor_words_from_file("./profanity.txt")
+
+DISCORD_BOT_TOKEN=os.getenv('DISCORD_BOT_TOKEN')
 
 client=discord.Client(intents=discord.Intents.all())
 
@@ -17,6 +27,10 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello i am python bot')
+    #profanity message check and removing the message
+    if profanity.contains_profanity(message.content):
+        await message.delete()
+        await message.channel.send(f'You cannot use that word here @{message.author}')
 
 
-client.run('put-token-from-env')
+client.run(DISCORD_BOT_TOKEN)
